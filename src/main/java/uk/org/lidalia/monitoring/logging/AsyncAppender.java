d44340d05b72dcae4db533797fdce3e799dcae8c
Robert Elliot <rob@lidalia.org.uk>
2011-04-08 08:14:11 +0100

    Adding an example async appender decorator

---
+++ src/main/java/uk/org/lidalia/monitoring/logging/AsyncAppender.java
@@ -0,0 +1,26 @@
+package uk.org.lidalia.monitoring.logging;
+
+import ch.qos.logback.classic.spi.ILoggingEvent;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class AsyncAppender extends DecoratingAppender {
+
+	private final ExecutorService executor = Executors.newCachedThreadPool();
+
+	@Override
+	protected void append(final ILoggingEvent eventObject) {
+		executor.execute(new Runnable() {
+			@Override
+			public void run() {
+				decoratedAppender.doAppend(eventObject);
+			}
+		});
+	}
+
+	@Override public void stop() {
+		executor.shutdown();
+		super.stop();
+	}
+}
---
+++ src/main/java/uk/org/lidalia/monitoring/logging/ExecutorServiceFactory.java
@@ -0,0 +1,10 @@
+package uk.org.lidalia.monitoring.logging;
+
+import java.util.concurrent.ExecutorService;
+
+public class ExecutorServiceFactory {
+
+	public ExecutorService build() {
+		return null;
+	}
+}
